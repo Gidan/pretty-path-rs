82cb2e297861cef1dfc4f1cdc706cb76e992c3ec
Andrea Amatucci <amatucci.andrea@gmail.com>
2024-01-04 18:12:50 +0100

    add: highlight search word

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
+use colored::Colorize;
 
 fn main() {
-
+    println!("Hello {}", "World".bright_yellow().bold());
     match env::var("PATH") {
         Ok(val) => pretty_print(&val),
         Err(e) => println!("couldn't interpret: {e}"),
@@ -10,7 +11,16 @@ fn main() {
 }
 
 fn pretty_print(val : &str) {
-    let nval = val.replace(":", "\n");
-    println!("{}", nval);
+    let args: Vec<String> = env::args().collect();
+    let mut toPrint : &str = val;
+    if args.len() > 1 {
+        let search = &args[1].as_str();
+        let highlighted = search.red().to_string();
+        let whole = toPrint.replace(search, highlighted.as_str());
+        toPrint = whole.as_str();
+        println!("{}", toPrint.replace(":", "\n"));
+    } else {
+        println!("{}", toPrint.replace(":", "\n"));
+    }
 
 }
